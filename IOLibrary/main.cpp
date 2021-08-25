2983b74a564957af87402a13a34a579ea7704a07
Roberts <am003734@main.intgin.net>
2021-08-24 21:17:09 -0700

    Initial commit

--- IOLibrary/main.cpp
+++ IOLibrary/main.cpp
@@ -11,8 +11,8 @@ int main(int argc, char *argv[])
     std::cout << "the sum of " << v1 << " and " << v2
               << " is " << v1 + v2 << std::endl;
 
-    // we don't need an event loop so we can comment out the
-    // following line. This will allow pressing the return
+    // If we don't need an event loop so we can comment out
+    // the following line. This will allow pressing the return
     // key to end the program.
     return a.exec();
 }
