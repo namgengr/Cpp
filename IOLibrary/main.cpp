e3d30c4998fb87e596a30cb74c492a662c7904a9
Roberts <am003734@main.intgin.net>
2021-08-24 21:48:48 -0700

    initial commit

--- IOLibrary/main.cpp
+++ IOLibrary/main.cpp
@@ -10,6 +10,19 @@ int main(int argc, char *argv[])
     std::cin >> v1 >> v2;
     std::cout << "the sum of " << v1 << " and " << v2
               << " is " << v1 + v2 << std::endl;
+    std::cout << "the product of " << v1 << " and " << v2
+              << " is " << v1 * v2 << std::endl;
+    std::cout << "the difference of " << v1 << " and " << v2
+              << " is " << v1 - v2 << std::endl;
+
+    // using separate statements to print each operand
+    std::cout << "the sum of ";
+    std::cout << v1;
+    std::cout << " and ";
+    std::cout << v2;
+    std::cout << " is ";
+    std::cout << v1 + v2;
+    std::cout << std::endl;
 
     // If we don't need an event loop so we can comment out
     // the following line. This will allow pressing the return
