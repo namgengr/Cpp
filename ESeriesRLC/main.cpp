b2ac95fd3fa8bcc3221a7728bf9477c0c2c51c39
Roberts <am003734@main.intgin.net>
2021-09-09 08:32:24 -0700

    added notes

--- ESeriesRLC/main.cpp
+++ ESeriesRLC/main.cpp
@@ -5,6 +5,10 @@
 /*
  * Name: Tom Roberts
  * Subject: Derivation of preferred RLC standard values
+ * Notes:   1. Use RoundToDecimalPlaces located in this same repository
+ *              to truncate the eSeries values.
+ *          2. E48 and E96 values truncate to two decimal places and
+ *              all others truncate to one decimal place.
  */
 
 const std::vector<double> eSeries(const int m)
@@ -20,6 +24,7 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+    // E6, E12 and E24 series truncate to one decimal place
     std::cout << "E6 Standard values, 20%:" << std::endl;
     const std::vector<double> E6 = eSeries(6);
     for(size_t n = 0; n < E6.size(); n++)
@@ -30,8 +35,10 @@ int main(int argc, char *argv[])
     for(size_t n = 0; n < E12.size(); n++)
         std::cout << std::setprecision(2) << E12[n] << std::endl;
 
-    // use the same routine to compute E24 (5%), E48 (2%) and
-    // E96 (1%) standard values.
+    /* use the same routine to compute E24 (5%), E48 (2%) and
+     * E96 (1%) standard values. E48 and E96 truncate to two
+     * decimal places.
+     */
 
     return a.exec();
 }
