d9376c3a8a1ecf20668d4c163a7ff79c91718ea8
Roberts <am003734@main.intgin.net>
2021-09-10 21:21:54 -0700

    minor formatting

--- ForStatement/main.cpp
+++ ForStatement/main.cpp
@@ -13,13 +13,12 @@ int main(int argc, char *argv[])
 
     int sum = 0;
     // sum values 1 through 10 inclusive
-    for (int val = 1; val <= 10; ++val)
-    {
+    for (int val = 1; val <= 10; ++val) {
         // note: since our body is one line then we can
-        // omit the curly braces.
+        // omit the curly braces if desired
         sum += val; // equivalent to sum = sum + val
     }
-    qDebug() << "Sum of 1 to 10 inclusive is " << sum;
+    qDebug() << "Sum of 1 to 10 inclusive is" << sum;
 
     return a.exec();
 }
